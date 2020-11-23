5136b7b0b08d265a7328ecf0ac233fa4dcd9547e
soun1920 <36147122+soun1920@users.noreply.github.com>
2020-11-23 09:56:38 +0900

    ps4に対応

--- bot.py
+++ bot.py
@@ -23,25 +23,25 @@ c = connection.cursor()
 
 #c.execute("create table rankPoint (discord_id intger , PlayerName text , old_RP integer)")
 
-class command():
-
-    @connect.command()
-    async def stat(ctx,user_name,platform):
+class commands:
+    @connect.group(invoke_without_command=True)
+    async def stat(ctx,user_name):
         connection = sqlite3.connect("rankPoint_DB")
         c = connection.cursor()
         
-        if platfrom =="ps4":
-            platform_out="psn"
-        platform_out = "pc"
         params = {"TRN-Api-Key":(os.environ["APEX_KEY"])}
-        endpoint = "profile/"+platform_out+"/"+str(user_name)
+        endpoint = "profile/"+"origin"+"/"+str(user_name)
         session = requests.Session()
         req = session.get(base_url+endpoint,params=params)
         req.close()
         req_data = json.loads(req.text)
 
-        
-        RP =  req_data['data']['segments'][0]['stats']['rankScore']['value']
+        try:
+            RP =  req_data['data']['segments'][0]['stats']['rankScore']['value']
+        except KeyError:
+            await ctx.send("アカウントが存在しません")
+            return 
+
         user_data = (ctx.author.id , user_name , RP )
         user_id = ctx.author.id
         
@@ -55,7 +55,55 @@ class command():
             c.execute('INSERT INTO rankPoint (discord_id, PlayerName, old_RP) values (?,?,?)', user_data)
         else:
             old_point = data_base[2]
-            c.execute(f"update rankPoint set old_RP={RP},where discord_id={user_id} AND PlayerName='{user_name}'")
+            c.execute(f"update rankPoint set old_RP={RP} where discord_id={user_id} AND PlayerName='{user_name}'")
+
+        #c.execute("select * from rankPoint")
+
+        change = RP - old_point
+        if change<0:
+            sign = "-"
+        elif change == 0:
+            sign = ""
+        else:
+            sign = "+"
+        
+        await ctx.send(f"{user_name}  の現在のRPは {RP}({sign}{change}) です " )
+        
+        connection.commit()
+        c.close()
+        connection.close()
+    @stat.command()
+    async def ps4(ctx,user_name):
+        connection = sqlite3.connect("rankPoint_DB")
+        c = connection.cursor()
+        
+        params = {"TRN-Api-Key":(os.environ["APEX_KEY"])}
+        endpoint = "profile/"+"psn"+"/"+str(user_name)
+        session = requests.Session()
+        req = session.get(base_url+endpoint,params=params)
+        req.close()
+        req_data = json.loads(req.text)
+
+        try:
+            RP =  req_data['data']['segments'][0]['stats']['rankScore']['value']
+        except KeyError:
+            await ctx.send("アカウントが存在しません")
+            return 
+
+        user_data = (ctx.author.id , user_name , RP )
+        user_id = ctx.author.id
+        
+        c.execute(f"SELECT * FROM rankPoint WHERE discord_id={user_id} AND PlayerName='{user_name}'",)
+
+        data_base = c.fetchone()
+        
+        db_all = c.fetchall()
+        if len(db_all) == 0:
+            old_point = RP
+            c.execute('INSERT INTO rankPoint (discord_id, PlayerName, old_RP) values (?,?,?)', user_data)
+        else:
+            old_point = data_base[2]
+            c.execute(f"update rankPoint set old_RP={RP} where discord_id={user_id} AND PlayerName='{user_name}'")
 
         #c.execute("select * from rankPoint")
 
@@ -91,11 +139,9 @@ class events:
         guilds=connect.guilds
         await connect.change_presence(activity=discord.Game(name="[/stat]   server: "+str(len(guilds))))
         print("server remove" + guild.name)
-
-
-
-
-
+    async def on_command_error(ctx,error):
+        print(ctx,error)
+        pass
 
 
 connect.run(discord_api)
