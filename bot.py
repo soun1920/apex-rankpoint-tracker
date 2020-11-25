4fd6f9f9d3465133e4b1dc7855202456796029f5
soun1920 <36147122+soun1920@users.noreply.github.com>
2020-11-25 20:57:28 +0900

    最適化

--- bot.py
+++ bot.py
@@ -41,28 +41,30 @@ class commands:
         try:
             RP =  req_data['data']['segments'][0]['stats']['rankScore']['value']
         except KeyError:
+            params = {"TRN-Api-Key":(os.environ["APEX_KEY"])}
+            endpoint = "profile/"+"psn"+"/"+str(user_name)
+            session = requests.Session()
+            req = session.get(base_url+endpoint,params=params)
+            req.close()
+            req_data = json.loads(req.text)
             try:
+                RP =  req_data['data']['segments'][0]['stats']['rankScore']['value']
+            except KeyError:
                 params = {"TRN-Api-Key":(os.environ["APEX_KEY"])}
-                endpoint = "profile/"+"psn"+"/"+str(user_name)
+                endpoint = "profile/"+"xbox"+"/"+str(user_name)
                 session = requests.Session()
                 req = session.get(base_url+endpoint,params=params)
                 req.close()
                 req_data = json.loads(req.text)
-                RP =  req_data['data']['segments'][0]['stats']['rankScore']['value']
-            except KeyError:
                 try:
-                    params = {"TRN-Api-Key":(os.environ["APEX_KEY"])}
-                    endpoint = "profile/"+"xbox"+"/"+str(user_name)
-                    session = requests.Session()
-                    req = session.get(base_url+endpoint,params=params)
-                    req.close()
-                    req_data = json.loads(req.text)
                     RP =  req_data['data']['segments'][0]['stats']['rankScore']['value']
                 except KeyError:
                     await ctx.send("アカウント名が間違っているか、存在しません")
 
-
         user_data = (ctx.author.id , user_name , RP )
+
+
+
         user_id = ctx.author.id
         
         c.execute(f"SELECT * FROM rankPoint WHERE discord_id={user_id} AND PlayerName='{user_name}'",)
@@ -71,8 +73,11 @@ class commands:
         
         db_all = c.fetchall()
         if len(db_all) == 0:
+            await ctx.send(len(db_all))
             old_point = RP
             c.execute('INSERT INTO rankPoint (discord_id, PlayerName, old_RP) values (?,?,?)', user_data)
+            c.execute('INSERT INTO rankPoint (discord_id, PlayerName, old_RP) values (?,?,?)', user_data)
+
         else:
             old_point = data_base[2]
             c.execute(f"update rankPoint set old_RP={RP} where discord_id={user_id} AND PlayerName='{user_name}'")
