38ee733df150c8bc50c66aba73c001d81df6d20b
soun1920 <soun29300@gmail.com>
2022-04-21 00:19:15 +0900

    [add] Logging

--- src/db.py
+++ src/db.py
@@ -2,11 +2,14 @@ import asyncpg
 from dotenv import load_dotenv
 
 import os
+import logging
 from typing import Optional, Any
 
 load_dotenv("../.env")
 dsn = os.environ["POSTGRES"]
 table_name = os.environ["TABLE_NAME"]
+
+logger = logging.getLogger(__name__)
 
 
 class SQL:
@@ -32,6 +35,8 @@ class SQL:
 
     async def create_pool(self):
         self.pool = await asyncpg.create_pool(dsn)
+        if self.pool is None:
+            logger.warning("SQLへの接続が失敗しました")
 
     async def select(self) -> asyncpg.Record:
         return await self.pool.fetch(f'SELECT * FROM {table_name} WHERE id=$1 and name=$2', self.id, self.name)
