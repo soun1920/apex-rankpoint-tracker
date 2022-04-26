25545089139f160162ec2bb920e7f100520e1c3b
soun1920 <soun29300@gmail.com>
2022-04-26 21:09:29 +0900

    [Change] poolに直接クエリを投げずにconnectionを作ってクエリを投げる方式に変更

--- src/db.py
+++ src/db.py
@@ -14,22 +14,27 @@ logger = logging.getLogger(__name__)
 
 class SQL:
     def __init__(
-            self, id: int,
-            name: str,
-            platform: Optional[str] = None,
-            rankseason: Optional[str] = None
+        self,
+        id: int,
+        name: str,
+        platform: Optional[str] = None,
+        rankseason: Optional[str] = None,
     ) -> None:
         self.id = id  # discord user id
         self.name = name  # apex player name
         self._platform: Optional[str] = platform
         self._rankseason: Optional[str] = rankseason
+        self._rankpoint = None
         self.pool = None
 
     @classmethod
-    async def init(cls, id: int,
-                   name: str,
-                   platform: Optional[str] = None,
-                   rankseason: Optional[str] = None):
+    async def init(
+        cls,
+        id: int,
+        name: str,
+        platform: Optional[str] = None,
+        rankseason: Optional[str] = None,
+    ):
         cls.pool = await asyncpg.create_pool(dsn)
         return cls(id, name, platform, rankseason)
 
@@ -39,16 +44,24 @@ class SQL:
             logger.warning("SQLへの接続が失敗しました")
 
     async def select(self) -> asyncpg.Record:
-        return await self.pool.fetch(f'SELECT * FROM {table_name} WHERE id=$1 and name=$2', self.id, self.name)
+        async with self.pool.acquire() as conn:
+            return await conn.fetch(
+                f"SELECT * FROM {table_name} WHERE id=$1 and name=$2", self.id, self.name
+            )
 
     async def insert(
-        self, id: int, name: str,
-        platform: str, point: int, rankseason: str, dt
+        self, id: int, name: str, platform: str, point: int, rankseason: str, dt
     ) -> str:
-        return await self.pool.execute(
-            f'INSERT INTO {table_name} VALUES ($1,$2,$3,$4,$5,$6)',
-            id, name, platform, point, rankseason, dt
-        )
+        async with self.pool.acquire() as conn:
+            return await conn.execute(
+                f"INSERT INTO {table_name} VALUES ($1,$2,$3,$4,$5,$6)",
+                id,
+                name,
+                platform,
+                point,
+                rankseason,
+                dt,
+            )
 
     async def is_exists(self) -> bool:
         res = await self.select()
@@ -58,9 +71,20 @@ class SQL:
             return True
 
     async def latest_data(self) -> asyncpg.Record:  # 直近のデータ
-        return await self.pool.fetchrow(
-            f'SELECT * FROM {table_name} WHERE id=$1 and name=$2 and datetime=(SELECT MAX(datetime) FROM test)', self.id, self.name)
+        async with self.pool.acquire() as conn:
+            record = await conn.fetchrow(
+                f"SELECT * FROM {table_name} WHERE id=$1 and name=$2 and datetime=(SELECT MAX(datetime) FROM test)",
+                self.id,
+                self.name,
+            )
+        self._platform = record["platform"]
+        self._rankpoint = record["point"]
+        return record
 
     @property
     def platform(self):
-        return
+        return self._platform
+
+    @property
+    def point(self):
+        return self._rankpoint
